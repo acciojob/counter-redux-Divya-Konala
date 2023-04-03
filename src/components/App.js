32589f599d4ad56bd292b72156e4164545b6828a
Divya-Konala <87460968+Divya-Konala@users.noreply.github.com>
2023-04-03 10:40:26 +0530

    useState counter

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [count,setCount]=useState(0);
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>{count}</h1>
+        <button onClick={()=>setCount(count+1)}>Increment</button>
+        <button onClick={()=>setCount(count-1)}>Decrement</button>
     </div>
   )
 }
